e6c9b742d3a7461d151fe230eea903b9da05758e
Vasiliu Adelin <vasiliu.gheorghe.adelin@gmail.com>
2022-05-10 22:13:06 -0700

    chore: update manifest

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,7 +1,3 @@
-/* eslint-disable prefer-regex-literals */
-/* eslint-disable no-underscore-dangle */
-/* eslint-disable no-restricted-globals */
-
 // This service worker can be customized!
 // See https://developers.google.com/web/tools/workbox/modules
 // for the list of available Workbox modules, or add any other
